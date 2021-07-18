60401c0eaba7bbb0340d25dbe61e5f3b60286398
YusukeKinouchi <kanitama3980@outlook.jp>
2021-07-18 20:22:59 +0900

    fix HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,21 @@ use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
     public function index()
     {
         $user = Auth::user();
@@ -22,4 +37,3 @@ class HomeController extends Controller
         ]);
     }
 }
-//herokuテスト用のコメント
