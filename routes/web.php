d6ce223ca98f15bd0e6cc3bf1d9084061c74323d
YusukeKinouchi <kanitama3980@outlook.jp>
2021-07-18 20:48:33 +0900

    fix task.php

--- routes/web.php
+++ routes/web.php
@@ -14,20 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group(['middleware' => 'auth'], function(){
-    
-    Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
-    Route::post('/folders/create', 'FolderController@create');
+    Route::get('/','App\Http\Controllers\HomeController@index')->name('home');
 
-    Route::get('/', 'HomeController@index')->name('home');
+    Route::get('/folders/create', 'App\Http\Controllers\FolderController@showCreateForm')->name('folders.create');
+    Route::post('/folders/create', 'App\Http\Controllers\FolderController@create');
 
     Route::group(['middleware' => 'can:view,folder'], function(){
-        Route::get('/folders/{folder}/tasks', 'TaskController@index')->name('tasks.index');
+        Route::get('/folders/{folder}/tasks', 'App\Http\Controllers\TaskController@index')->name('tasks.index');
 
-        Route::get('/folders/{folder}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
-        Route::post('/folders/{folder}/tasks/create', 'TaskController@create');
+        Route::get('/folders/{folder}/tasks/create', 'App\Http\Controllers\TaskController@showCreateForm')->name('tasks.create');
+        Route::post('/folders/{folder}/tasks/create', 'App\Http\Controllers\TaskController@create');
 
-        Route::get('/folders/{folder}/tasks/{task}/edit', 'TaskController@showEditForm')->name('tasks.edit');
-        Route::post('/folders/{folder}/tasks/{task}/edit', 'TaskController@edit');
+        Route::get('/folders/{folder}/tasks/{task_id}/edit', 'App\Http\Controllers\TaskController@showEditForm')->name('tasks.edit');
+        Route::post('/folders/{folder}/tasks/{task_id}/edit', 'App\Http\Controllers\TaskController@edit');
     });
 });
 
